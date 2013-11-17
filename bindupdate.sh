d7f5fa6cb194f9916cf9ca93eb9a8d1d359cea1d
Michael <Michael@ftwnet.co.uk>
2013-11-17 22:51:46 +0000

    Added the right color

--- bindupdate.sh
+++ bindupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo -e "\E[33;40m[*] Checking if Bind9 is installed.\e[0m"
 if [ -f /etc/init.d/bind9 ];  then
-  echo "\E[32;40m Bind is installed, Cool! \e[0m"
+  echo -e "\E[32;40m[*] Bind9 is not installed [*]\e[0m"
   echo -e "\E[32;40m[*] Updating your bind9 ad-block list Okay! [*]\e[0m"
   sleep 2
   wget -O /etc/bind/named.conf.ads  "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=bindconfig;showintro=0&mimetype=plaintext"
@@ -10,7 +10,7 @@ if [ -f /etc/init.d/bind9 ];  then
   /etc/init.d/bind9 reload
   exit 1
 else 
-  echo -e "Bind9 is not installed"
+  echo -e "\e[0;31m Bind9 is not installed \e[0;31m"
   exit 1
 fi
 
