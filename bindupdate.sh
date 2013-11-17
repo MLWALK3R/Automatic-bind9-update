370e03f57e9042e5e3f0d76f80386c21693db457
Michael <Michael@ftwnet.co.uk>
2013-11-17 22:53:02 +0000

    remove not **

--- bindupdate.sh
+++ bindupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo -e "\E[33;40m[*] Checking if Bind9 is installed.\e[0m"
 if [ -f /etc/init.d/bind9 ];  then
-  echo -e "\E[32;40m[*] Bind9 is not installed [*]\e[0m"
+  echo -e "\E[32;40m Bind9 is installed!!! \e[0m"
   echo -e "\E[32;40m[*] Updating your bind9 ad-block list Okay! [*]\e[0m"
   sleep 2
   wget -O /etc/bind/named.conf.ads  "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=bindconfig;showintro=0&mimetype=plaintext"
